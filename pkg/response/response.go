1177bb78f6ea25d17464f5dd56797d20c863f924
Sonic Ma <sonic.ma@outlook.com>
2023-11-20 21:17:24 +0800

    fix:views

--- pkg/response/response.go
+++ pkg/response/response.go
@@ -2,6 +2,12 @@ package response
 
 import "github.com/gin-gonic/gin"
 
+// PageResult 列表数据返回
+type PageResult struct {
+	Total int64       `json:"total"`
+	List  interface{} `json:"list"`
+}
+
 // JsonRes 数据返回通用JSON数据结构
 type JsonRes struct {
 	Code    int         `json:"code"`    // 错误码((0:成功, 1:失败, >1:错误码))
@@ -11,18 +17,32 @@ type JsonRes struct {
 
 // Json 返回标准JSON数据
 func Json(c *gin.Context, httpCode int, message string, data interface{}) {
-	c.JSON(httpCode, JsonRes{
+	resp := JsonRes{
 		Code:    0,
 		Message: message,
-		Data:    data,
-	})
+	}
+
+	if data == nil {
+		resp.Data = struct{}{}
+	} else {
+		resp.Data = data
+	}
+
+	c.JSON(httpCode, resp)
 }
 
 // JsonExit 返回标准JSON数据并退出当前HTTP执行函数。
 func JsonExit(c *gin.Context, httpCode int, message string, data interface{}) {
-	c.JSON(httpCode, JsonRes{
+	resp := JsonRes{
 		Code:    -1,
 		Message: message,
-		Data:    data,
-	})
+	}
+
+	if data == nil {
+		resp.Data = struct{}{}
+	} else {
+		resp.Data = data
+	}
+
+	c.JSON(httpCode, resp)
 }
